3697496d3c6690d09996f9512d456a54a064ec9f
Guilherme Barbosa Ochikubo <37846911+gbochikubo@users.noreply.github.com>
2019-04-14 18:25:36 -0300

    Criando classeB.java

---
+++ classB.java
@@ -0,0 +1,29 @@
+    
+public class classeB{
+  private int B1;
+  private float B2;
+  
+  
+  public int getB1{
+    return B1;
+  }
+  public float getB2{
+    return B2;
+  }
+  
+  public void setA1(int B1){
+   this.A1 = B1;
+  }
+  
+  public void setA2(float B2){
+    this.A2 = B2;
+  }
+  
+  public void MB1(){
+   System.out.println("MB1");
+  }
+  
+  public void MB2(){
+    System.out.println("MB2");
+  }
+}
