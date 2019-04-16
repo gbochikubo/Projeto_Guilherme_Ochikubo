af5db57c1326905f51607f8ce3297565ac55a418
Guilherme <guilhermeochikubo@gmail.com>
2019-04-16 17:39:34 -0300

    arrumando as alteraçoes apos o merge

--- classD.java
+++ classD.java
@@ -9,14 +9,13 @@ public void MD1(){
 public void MD2(){
 	System.out.println("MD2");
 }
-<<<<<<< HEAD
 
 public void MD3(){
 	System.out.println("MD3");
 }
-=======
+
 public void MD4(){
 	System.out.println("MD4");
 }
 	
->>>>>>> 8bc986da20599ca16fb0d4694321d1e891a8973f
+
